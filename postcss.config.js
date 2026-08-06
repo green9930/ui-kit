7d3d9a39f0c37e9cf975082527085845e76769e9
green9930 <69451758+green9930@users.noreply.github.com>
2026-08-06 11:21:12 +0900

    chore: scaffold vite library build pipeline

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import globalData from '@csstools/postcss-global-data'
+import customMedia from 'postcss-custom-media'
+
+export default {
+  plugins: [
+    globalData({ files: ['./src/styles/media.css'] }),
+    customMedia(),
+  ],
+}
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Size = 'sm' | 'md' | 'lg'
+
+export type ColorScheme =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'warning'
+  | 'success'
+
+export type Variant = 'solid' | 'outline' | 'ghost' | 'link'
